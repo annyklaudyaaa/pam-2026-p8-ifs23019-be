741498ca6825add6b59c913ede0671c1a2f19e80
anny klaudya <annymusa02@gmail.com>
2026-04-18 10:08:37 +0700

    Update Routing.kt

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -50,7 +50,7 @@ fun Application.configureRouting() {
 
     routing {
         get("/") {
-            call.respondText("API telah berjalan. Dibuat oleh Aisah Sipahutar.")
+            call.respondText("API telah berjalan. Dibuat oleh Anny Klaudya.")
         }
 
         // Route Auth
